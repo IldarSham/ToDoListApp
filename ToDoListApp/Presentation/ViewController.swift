33ba29b194e5c70cf687ba98e3f294702f3146a4
IldarSham <68460564+IldarSham@users.noreply.github.com>
2025-08-04 23:23:18 +0500

    Initial commit

---
+++ ToDoListApp/Presentation/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Ildar Shamsullin on 04.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
---
+++ ToDoListAppTests/ToDoListAppTests.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListAppTests.swift
+//  ToDoListAppTests
+//
+//  Created by Ildar Shamsullin on 04.08.2025.
+//
+
+import XCTest
+@testable import ToDoListApp
+
+final class ToDoListAppTests: XCTestCase {
+  
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+}
