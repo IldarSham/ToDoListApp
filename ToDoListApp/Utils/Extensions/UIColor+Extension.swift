aba505309e21b6585b7f46c60e9f3ee9d22d6267
IldarSham <68460564+IldarSham@users.noreply.github.com>
2025-08-06 00:27:35 +0500

    Refactor Utils: move ReuseIdentifiable to Extensions, add UIColor+Extension

---
+++ ToDoListApp/Utils/Extensions/UIColor+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extension.swift
+//  ToDoListApp
+//
+//  Created by Ildar Shamsullin on 05.08.2025.
+//
+
+import UIKit
+
+public extension UIColor {
+  static let primaryGrayColor = UIColor(red: 141/255, green: 141/255, blue: 142/255, alpha: 1)
+  static let primaryDarkGrayColor = UIColor(red: 39/255, green: 39/255, blue: 40/255, alpha: 1)
+}
