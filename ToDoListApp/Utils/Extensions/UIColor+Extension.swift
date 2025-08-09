dafd513cb50ffd6f7c8553e68426dd44f10bb46d
IldarSham <68460564+IldarSham@users.noreply.github.com>
2025-08-10 00:40:15 +0500

    Add UIColor extension

--- ToDoListApp/Utils/Extensions/UIColor+Extension.swift
+++ ToDoListApp/Utils/Extensions/UIColor+Extension.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 public extension UIColor {
-  static let primaryGrayColor = UIColor(red: 141/255, green: 141/255, blue: 142/255, alpha: 1)
+  static let primaryGrayColor = UIColor(red: 124/255, green: 124/255, blue: 124/255, alpha: 1)
+  static let primaryLightGrayColor = UIColor(red: 141/255, green: 141/255, blue: 142/255, alpha: 1)
   static let primaryDarkGrayColor = UIColor(red: 39/255, green: 39/255, blue: 40/255, alpha: 1)
 }
