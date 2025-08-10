30843b859ae7b76965a29153942e0f467bc5fec6
IldarSham <68460564+IldarSham@users.noreply.github.com>
2025-08-10 12:03:16 +0500

    Add Alertable extension

--- ToDoListApp/Utils/Extensions/Alertable.swift
+++ ToDoListApp/Utils/Extensions/Alertable.swift
@@ -5,4 +5,20 @@
 //  Created by Ildar Shamsullin on 10.08.2025.
 //
 
-import Foundation
+import UIKit
+
+protocol Alertable {
+  func presentAlert(title: String?, message: String?)
+}
+
+extension Alertable where Self: UIViewController {
+  func presentAlert(title: String?, message: String?) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    present(alertController, animated: true, completion: nil)
+  }
+}
+
+extension Alertable {
+  func presentAlert(title: String?, message: String?) {}
+}
